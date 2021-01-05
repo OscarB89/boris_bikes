9507583185a552935320a9558dfd64062b64da47
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-05 16:47:42 +0000

    Added test that expects DockingStation to repsond to release_bike method & defined release_bike method

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,2 +1,12 @@
 class DockingStation
+    def initialize
+    end
+
+    def release_bike
+      puts "hi"
+    end
+
 end
+
+test = DockingStation.new.release_bike
+puts test
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,2 +1,7 @@
+require 'boris_bikes'
+
 describe DockingStation do
+    it 'DockingStation to respond to the release_bike method' do
+        expect(DockingStation.new).to respond_to(release_bike)
+    end
 end
