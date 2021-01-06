46b7623d1188a5ce8669bee0f8b796b3ecd70e96
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-06 14:57:05 +0000

    Added a test for docking station to get a working bike, and functionality to pass it

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,9 +1,11 @@
+require "bike"
+
 class DockingStation
     def initialize
     end
 
     def release_bike
-
+      Bike.new
     end
 
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,7 +1,12 @@
 require 'boris_bikes'
 
 describe DockingStation do
-    it 'DockingStation to respond to the release_bike method' do
-        expect(DockingStation.new).to respond_to(:release_bike)
+
+    it { is_expected.to respond_to(:release_bike) }
+
+    it "gets a bike which is working" do
+      bike = DockingStation.new.release_bike
+      expect(bike.working?).to equal true
+
     end
 end
